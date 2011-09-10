d3cc2c7b028d3869f407c5e25c7cd4940c85d2ff
Nick Meharry <nick.meharry@gmail.com>
2011-09-10 12:00:30 -0700

    Automatically log the user in after activation.

--- registration/signals.py
+++ registration/signals.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login, get_backends
 from django.dispatch import Signal
 
 
@@ -6,3 +7,9 @@ user_registered = Signal(providing_args=["user", "request"])
 
 # A user has activated his or her account.
 user_activated = Signal(providing_args=["user", "request"])
+
+def log_in_user(sender, user, request, **kwargs):
+    backend = get_backends()[0] # A bit of a hack to bypass `authenticate()`.
+    user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
+    login(request, user)
+user_activated.connect(log_in_user)
