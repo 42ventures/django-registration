dbef89449cb00a952e8a51fdf855f69b79a448bb
Kenneth Falck <kennu@iki.fi>
2011-10-12 16:56:39 +0300

    Strip timezone from date_joined (returned by django-mongodb-engine)

--- registration/models.py
+++ registration/models.py
@@ -201,7 +201,7 @@ class RegistrationProfile(models.Model):
         """
         expiration_date = datetime.timedelta(days=settings.ACCOUNT_ACTIVATION_DAYS)
         return self.activation_key == self.ACTIVATED or \
-               (self.user.date_joined + expiration_date <= datetime.datetime.now())
+               (self.user.date_joined.replace(tzinfo=None) + expiration_date <= datetime.datetime.now())
     activation_key_expired.boolean = True
 
     def send_activation_email(self, site):
